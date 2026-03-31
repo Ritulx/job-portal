b81a9d48172f4f6c3d05ca25b1b184d6f115e56f
Ritulx <ritul042005@gmail.com>
2026-03-31 22:10:39 +0530

    fixing resume download3

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "https://job-portal-production-bbac.up.railway.app/api/v1/user";
+export const USER_API_ENDPOINT = "https://job-portal-production-bbac.up.railway.app/api/users";
 export const JOB_API_ENDPOINT = "https://job-portal-production-bbac.up.railway.app/api/job";
 export const APPLICATION_API_ENDPOINT = "https://job-portal-production-bbac.up.railway.app/api/application";
 export const COMPANY_API_ENDPOINT = "https://job-portal-production-bbac.up.railway.app/api/company";
